88e6ae0bfec7f7f516ff47e5a46c23b7dc2e8a9f
Yau1heni <lodbrok9@gmail.com>
2022-11-21 03:13:37 +0300

    hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -6,27 +6,20 @@ import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
 
-/*
-* 1 - в файле loadingReducer.ts дописать типы и логику
-* 2 - получить isLoading из редакса
-* 3 - дописать функцию setLoading
-* 4 - сделать стили в соответствии с дизайном
-* */
-
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
 
+    const isLoading = useSelector<AppStoreType, any>(state => state.loading.isLoading)
+    const dispatch = useDispatch()
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
-
-        // setTimeout
+        dispatch(loadingAC(true))
+        setTimeout(()=> {
+            dispatch(loadingAC(false))
+        }, 1500)
     }
 
     return (
         <div id={'hw10'}>
             <div className={s2.hwTitle}>Homework #10</div>
-
             <div className={s2.hw}>
                 {isLoading ? (
                     <div id={'hw10-loading'}>
